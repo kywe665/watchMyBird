d7372b8d13ea287094583563638c52df62469d63
Kyle Weller <kywe665@gmail.com>
2013-10-12 23:35:15 -0700

    Finished sharing buttons, put in override and gen detection

--- birdScript.js
+++ birdScript.js
@@ -1,20 +1,34 @@
 ﻿(function () {
+    var feedOverride = '665Override';
     $(document).ready(function () {
         $('.js-feed').on('click', function () {
-            var code = $('#feedCode').val();
-            console.log(code);
-            decode = parseInt(code, 19);
-            $.get("http://67.170.74.247:6547/feedTheBird?feedCode=" + decode + "&now=" + Date.now(), function (data, status) {
-                console.log(status);
-                console.log(data);
-            });
+            feedRequest();
         });
-        $('#feedCode').on('keypress', function (evt) {
-            console.log('key');
+        $('#feedCode').on('keyup', function (evt) {
             var keycode = evt.keyCode || evt.which;
+            if (keycode == 13) {
+                feedRequest();
+            }
             if (keycode === 75 && evt.shiftKey) {
                 $('#feedCode').val(Date.now().toString(19));
             }
+            if (feedOverride.indexOf($(this).val()) != -1) {
+                $(this).attr('type', 'password');
+            } else {
+                $(this).attr('type', 'text');
+            }
         });
     });
+    function feedRequest() {
+        var code = $('#feedCode').val();
+        if (code != feedOverride) {
+            decode = parseInt(code, 19);
+        } else {
+            decode = Date.now();
+        }
+        $.get("http://67.170.74.247:6547/feedTheBird?feedCode=" + decode + "&now=" + Date.now(), function (data, status) {
+            console.log(status);
+            console.log(data);
+        });
+    }
 })();
