9840a4c2724280a3ee8585048c4290afdad1f3d2
Kyle Weller <kywe665@gmail.com>
2013-10-26 01:44:49 -0700

    Added canvas backup if browser does not accept mjpg. Camera saves images at 3 fps over ftp to same domain. I then loop through these to display on the canvas.
    You get about 2 frames per second and it is pretty choppy...

--- birdScript.js
+++ birdScript.js
@@ -1,5 +1,12 @@
 ﻿(function () {
     var feedOverride = '665Override';
+    var isOpera = !!window.opera || navigator.userAgent.indexOf('Opera') >= 0;
+    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+    var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+    var isChrome = !!window.chrome;                          // Chrome 1+
+    var isIE = /*@cc_on!@*/false;                            // At least IE6
     $(document).ready(function () {
         $('.js-feed').on('click', function () {
             feedRequest();
@@ -18,6 +25,15 @@
                 $(this).attr('type', 'text');
             }
         });
+        $('#stream').attr('src', 'http://bit.ly/1ccY67W').load(function () {
+            //stream working
+        }).error(function (e) {
+            console.log(JSON.stringify(e));
+            alert(JSON.stringify(e));
+            if (!isChrome && !isFirefox) {
+                canvasBackup();
+            }
+        });
     });
     function feedRequest() {
         var code = $('#feedCode').val();
@@ -30,5 +46,23 @@
             console.log(status);
             console.log(data);
         });
+    }
+    function canvasBackup() {
+        //If the browser cannot stream mjpg, use canvas.
+        $('#canvasBackup').removeClass("css-hidden");
+        $('#stream').addClass("css-hidden");
+        var ctx = document.getElementById('canvasBackup').getContext('2d');
+        var img = new Image();
+        var count = 0;
+        img.onload = function () {
+            console.log('draw');
+            //TODO FIX CANVAS SIZE
+            ctx.drawImage(img, 0, 0, 534,400);
+        };
+        setInterval(function () {
+            console.log('again');
+            img.src = "http://kywe665.com/feedMyBird/birdStatus" + (count % 3 + 1) + ".jpg";
+            count++;
+        }, 450);
     }
 })();
